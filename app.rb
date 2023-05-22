b039b983fcce69f6cbcb45afe53310dd20c8b5f3
soutajo <roninghost42@gmail.com>
2023-05-22 18:04:50 +0300

    Init database, create table Posts

--- app.rb
+++ app.rb
@@ -2,6 +2,26 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
+
+def init_db
+	@db = SQLite3::Database.new 'bloggerspace.db'
+	@db.results_as_hash = true
+end
+
+before do
+	init_db
+end
+
+configure do
+	init_db
+	@db.execute 'CREATE TABLE IF NOT EXISTS "Posts"
+	(
+		"id" INTEGER PRIMARY KEY AUTOINCREMENT,
+		"created_date" DATE,
+		"content" TEXT
+	);'
+end
 
 get '/' do
 	erb "Welcome!</a>"			
@@ -9,4 +29,10 @@ end
 
 get '/new' do
 	erb :new
+end
+
+post '/new' do
+	content = params[:content]
+
+	erb "You typed #{content}"
 end
